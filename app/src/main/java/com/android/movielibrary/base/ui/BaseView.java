0bba9408aa266d14043790a90210020361e12c39
aamir2590 <aamirkhn546@gmail.com>
2018-04-01 16:30:40 +0530

    Change BaseView and BasePresenter class structure.

--- app/src/main/java/com/android/movielibrary/base/ui/BaseView.java
+++ app/src/main/java/com/android/movielibrary/base/ui/BaseView.java
@@ -4,5 +4,6 @@ package com.android.movielibrary.base.ui;
  * Created by aamir on 30/03/18.
  */
 
-public interface BaseView {
+public interface BaseView<T extends BasePresenter> {
+    void setPresenter(T presenter);
 }
