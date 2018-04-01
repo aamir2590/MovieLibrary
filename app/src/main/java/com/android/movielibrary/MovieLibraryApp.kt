2b7e17d61458d2454014e4d3738b0d41ef044fd1
aamir2590 <aamirkhn546@gmail.com>
2018-04-01 10:42:13 +0530

    Add MovieLibraryApp application class.

---
+++ app/src/main/java/com/android/movielibrary/MovieLibraryApp.kt
@@ -0,0 +1,13 @@
+package com.android.movielibrary
+
+import android.app.Application
+
+/**
+ * Created by aamir on 01/04/18.
+ */
+
+class MovieLibraryApp: Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
