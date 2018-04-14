d10a5cf4f448dcf575bb6597b094e60995caf7a4
aamir2590 <aamirkhn546@gmail.com>
2018-04-14 20:01:49 +0530

    Rename Genre class as GenresList.

---
+++ app/src/main/java/com/android/movielibrary/genre/data/GenresList.kt
@@ -0,0 +1,9 @@
+package com.android.movielibrary.genre.data
+
+/*
+ * Created by aamir on 10/04/18.
+ */
+data class GenresList(val genres: List<Genre>)
+
+data class Genre(val id: Int, val name: String)
+
