fabe6041d321b25b79bae512884ad30011ec2388
aamir2590 <aamirkhn546@gmail.com>
2018-04-14 11:51:41 +0530

    Remove unused import from GenreNetworkDataSource and GenreRepository.

--- app/src/main/java/com/android/movielibrary/genre/data/remote/GenreNetworkDataSource.kt
+++ app/src/main/java/com/android/movielibrary/genre/data/remote/GenreNetworkDataSource.kt
@@ -1,16 +1,8 @@
 package com.android.movielibrary.genre.data.remote
 
-import android.util.Log
-import com.android.movielibrary.BASE_URL
 import com.android.movielibrary.base.di.customscope.ActivityScope
 import com.android.movielibrary.genre.data.Genre
-import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import io.reactivex.Single
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.observers.DisposableSingleObserver
-import io.reactivex.schedulers.Schedulers
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Inject
 
 /**
--- app/src/main/java/com/android/movielibrary/genre/domain/GenreRepository.kt
+++ app/src/main/java/com/android/movielibrary/genre/domain/GenreRepository.kt
@@ -2,7 +2,6 @@ package com.android.movielibrary.genre.domain
 
 import com.android.movielibrary.genre.data.Genre
 import io.reactivex.Single
-import org.json.JSONObject
 
 /*
  * Created by aamir on 08/04/18.
